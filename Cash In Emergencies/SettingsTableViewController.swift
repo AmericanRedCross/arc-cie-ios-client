4c3f680f45799ea39ee1f548c86ccb3a09c313f1
Matthew Cheetham <matt@3sidedcube.com>
2017-08-25 08:45:42 +0100

    Stubs out handling of table view cells in settings screen

--- Cash In Emergencies/SettingsTableViewController.swift
+++ Cash In Emergencies/SettingsTableViewController.swift
@@ -17,5 +17,54 @@ class SettingsTableViewController: UITableViewController {
             navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
         }
     }
-
+    
+    /// Resets the user data so that the app works like a fresh install
+    func handleResetData() {
+        
+        let resetDataAlert = UIAlertController(title: "Reset All Data", message: "This will clear all progress and notes recorded in the app", preferredStyle: .alert)
+        
+        resetDataAlert.addAction(UIAlertAction(title: "Okay", style: .destructive, handler: { (action: UIAlertAction) in
+            
+            //Handle resetting data here when the controller exists
+            
+        }))
+        
+        resetDataAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(resetDataAlert, animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        switch indexPath.section {
+        case 0:
+            //Download section
+            switch indexPath.row {
+            case 0:
+                //Perform update
+                return
+            default:
+                return
+            }
+        case 1:
+            switch indexPath.row {
+            case 0:
+                //Play video
+                return
+            case 1:
+                handleResetData()
+                return
+            case 2:
+                //Change language
+                return
+            default:
+                return
+            }
+        default:
+            return
+        }
+        
+    }
 }
