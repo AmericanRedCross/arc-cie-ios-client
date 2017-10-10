f2e37db1e7f7e8443b762a74aa0dfdd968b0727e
Joel Trew <joel@3sidedcube.com>
2017-10-10 10:00:33 +0100

    Creates progress related files

---
+++ Cash In Emergencies/View Controllers/Progress/Cells/ProgressTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProgressTableViewCell.swift
+//  Cash In Emergencies
+//
+//  Created by Joel Trew on 10/10/2017.
+//  Copyright © 2017 3 SIDED CUBE. All rights reserved.
+//
+
+import UIKit
+
+class ProgressTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
