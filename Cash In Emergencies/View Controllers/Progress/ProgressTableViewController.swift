7196d9aa69ab46019a5d78a6bf52f2ae0a9329f4
Joel Trew <joel@3sidedcube.com>
2017-10-10 15:54:41 +0100

    Implements logic for creating progress viewmodels

--- Cash In Emergencies/View Controllers/Progress/ProgressTableViewController.swift
+++ Cash In Emergencies/View Controllers/Progress/ProgressTableViewController.swift
@@ -7,89 +7,134 @@
 //
 
 import UIKit
+import ThunderTable
+import ARCDM
 
-class ProgressTableViewController: UITableViewController {
+struct ProgressViewModel {
+    
+    var moduleHierarchy: String
+    var moduleTitle: String?
+    
+    var percentageComplete: Int {
+        return Int((Float(numberOfCompletedCriticalTools) / Float(numberOfCriticalTools)) * 100.0)
+    }
+    
+    var numberOfSubSteps: Int
+    var numberOfCompletedSubSteps: Int
+    
+    var numberOfCriticalTools: Int
+    var numberOfCompletedCriticalTools: Int
+}
 
+
+extension ProgressViewModel: Row {
+    
+    var cellClass: AnyClass? {
+        return ProgressTableViewCell.self
+    }
+    
+    var accessoryType: UITableViewCellAccessoryType? {
+        get {
+            return UITableViewCellAccessoryType.none
+        }
+        set {}
+    }
+    
+    func configure(cell: UITableViewCell, at indexPath: IndexPath, in tableViewController: TableViewController) {
+        guard let progressCell = cell as? ProgressTableViewCell else { return }
+        progressCell.criticalToolsValueLabel.text = "\(self.numberOfCompletedCriticalTools)/\(self.numberOfCriticalTools)"
+        progressCell.subStepsValueLabel.text = "\(numberOfCompletedSubSteps)/\(self.numberOfSubSteps)"
+        progressCell.hierarchyLabel.text = self.moduleHierarchy
+        progressCell.overallPercentageCompleteLabel.text = "\(self.percentageComplete)% COMPLETED"
+        progressCell.moduleLabel.text = self.moduleTitle
+        progressCell.moduleProgressView.progress = Double(self.percentageComplete)
+        progressCell.moduleProgressView.barColour = UIColor(hexString: "ed1b2e")
+    
+    }
+}
+
+class ProgressTableViewController: TableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
-        
-        tableView.register(UINib(nibName: "ProgressTableViewCell", bundle: nil), forCellReuseIdentifier: "progressTableViewCellIdentifier")
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 5
-    }
-
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "progressTableViewCellIdentifier", for: indexPath)
-
-        if let progressCell = cell as? ProgressTableViewCell {
-            return progressCell
+        if #available(iOS 11.0, *) {
+            self.navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
         }
-
-        return cell
+        
+        redraw()
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 110
+    func redraw() {
         
+        guard let modules = ModuleManager().modules else {
+            return
+        }
+        
+        var viewModels: [ProgressViewModel] = []
+        
+        for module in modules {
+            
+            let moduleTitle = module.moduleTitle
+            guard let hierarchy = module.metadata?["hierarchy"] as? String else { continue }
+            
+            let subSteps = module.directories?.flatMap({ (step) -> [Module] in
+                guard let stepDirectories = step.directories else { return [] }
+                return stepDirectories
+            }) ?? []
+            
+            let criticalTools = subSteps.flatMap({ (subStep) -> [Module] in
+                guard let subStepDirectories = subStep.directories else { return [] }
+                return subStepDirectories.flatMap({ (tool) -> Module? in
+                    
+                    if let isCritical = tool.metadata?["critical_path"] as? Bool,
+                        isCritical {
+                        return tool
+                    }
+                    
+                    return nil
+                })
+            })
+            
+            let numberOfSubSteps = subSteps.count
+            let numberOfCriticalTools = criticalTools.count
+            
+            
+            let counterClosure: ((Int, Module) -> Int) = { (completedCount, module) -> Int in
+                // Ensure we have an identifier to check the state of our step
+                guard let identifier = module.identifier else { return completedCount }
+                
+                // Check if the state is complete, otherwise return our current count
+                guard ProgressManager().checkState(for: identifier) else {
+                    return completedCount
+                }
+                
+                // If the state was true lets add on to our count and continue
+                return completedCount + 1
+                
+            }
+            
+            let completedSubSteps = subSteps.reduce(0, counterClosure)
+            let completedCriticalTools = criticalTools.reduce(0, counterClosure)
+            
+            
+
+           let viewModel = ProgressViewModel(moduleHierarchy: hierarchy, moduleTitle: moduleTitle, numberOfSubSteps: numberOfSubSteps, numberOfCompletedSubSteps: completedSubSteps, numberOfCriticalTools: numberOfCriticalTools, numberOfCompletedCriticalTools: completedCriticalTools)
+            
+            viewModels.append(viewModel)
+        }
+        
+        
+        self.data = [TableSection(rows: viewModels)]
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+        
+        // MARK: - Table view data sourc
+        override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 110
+            
+        }
+        
+        override var preferredStatusBarStyle: UIStatusBarStyle {
+            return .lightContent
+        }
 }
